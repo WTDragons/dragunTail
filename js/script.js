53996de3fc9701b39bd32d1488007b0a91dbaa75
WTDragons <108617862+WTDragons@users.noreply.github.com>
2023-02-22 01:12:11 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,134 +1,79 @@
-// zoom
-var zoom = 1;
-            function zoomIn() {
-              zoom += 1;
-              document.getElementById("myImg").style.transform = "scale(" + zoom + ")";
-            }
-            function zoomOut() {
-              if (zoom > 1) {
-                zoom -= 1;
-                document.getElementById("myImg").style.transform = "scale(" + zoom + ")";
-                return false;
-              } 
-            }
-          
-// modal
+//go top scroll
 $(document).ready(function(){
-  $('img').on('click', function(){
-    var img = $(this).data('img');
-    $('#modal-img').attr('src', img);
+  $('#go-top-btn').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
   });
 });
 
-// go top
-$(document).ready(function(){
-  $('#go-top-btn').click(function () {
-      $('body,html').animate({
-          scrollTop: 0
-      }, 800);
-      return false;
-  });
+//go top fade in and out
+$(document).scroll(function () {
+  let y = $(this).scrollTop();
+  if (y > 200) {
+    $('.goTopBtn').fadeIn();
+  } else {
+    $('.goTopBtn').fadeOut();
+  }
 });
 
 //Filter function
-$('.filter-btns button').click(function(){
-    //Remove active class from all buttons
-    $('.filter-btns button').removeClass('active');
-    //Add active class to clicked button
-    $(this).addClass('active');
-    var filterValue = $(this).attr('data-filter');
-    $('.card').show();
-    $('.card').not('[data-category="'+filterValue+'"]').hide();
-    if(filterValue=='all'){
-        $('.card').show();
-    }
-});
-
-//Filter function2
 $('.filter-btns button').click(function(){
   //Remove active class from all buttons
   $('.filter-btns button').removeClass('active');
   //Add active class to clicked button
   $(this).addClass('active');
   var filterValue = $(this).attr('data-filter');
-  $('.gallery_img').show();
-  $('.gallery_img').not('[data-category="'+filterValue+'"]').hide();
+  $('.filter-object').show();
+  $('.filter-object').not('[data-category="'+filterValue+'"]').hide();
   if(filterValue=='all'){
-      $('.gallery_img').show();
+    $('.filter-object').show();
   }
 });
-
-
-
-
-
-// local nav
-document.querySelectorAll('.local_nav .square').forEach(square => {
-  square.addEventListener('click', event => {
-    event.preventDefault();
-    const link = event.target.closest('a');
-    const sectionId = link.getAttribute('href');
-    const section = document.querySelector(sectionId);
-    section.scrollIntoView({ behavior: 'smooth' });
-  });
-}); 
-
-// // smooth moving, not that good anyway
-// $(document).ready(function(){
-//   $("a.smooth").click(function(event){
-//       event.preventDefault();
-//       var id = $(this).attr("href");
-//       $('html, body').animate({
-//           scrollTop: $(id).offset().top
-//       }, 800);
-//   });
-// });
-
-
-
+ 
 // copy text
-
-
-function copyText() {
-  var textToCopy = '暱稱\n委託類型\n交件期限\n付款方式\n預算\n委託資料\n委託內容\n作品權利\n用途\n規格\n特殊要求\n';
-  navigator.clipboard.writeText(textToCopy).then(function() {
-    alert("Text copied to clipboard!");
-    ;
+function copyText(){
+  var textToCopy = '暱稱\n委託類型\n截稿期限\n付款方式\n預算\n雙方授權範圍\n用途\n規格\n委託內容\n委託資料\n其他需求\n';
+  navigator.clipboard.writeText(textToCopy).then(function() {alert("成功複製了喔！好耶\n其實我覺得跳這個視窗出來滿煩的，要是您也覺得的話可以跟我說一下");
+  ;
   }, function(err) {
-      console.error("無法複製文本：", err);
+      console.error("複製失敗嗚嗚嗚", err);
   });
 }
 
-
-
-// $('.image-container img').click(function() {
-//   $(this).toggleClass('active');
-// });
-
-
-
-
-$(document).scroll(function () {
-  let y = $(this).scrollTop();
-  if (y > 200) {
-      $('.goTopBtn').fadeIn();
-  } else {
-      $('.goTopBtn').fadeOut();
-  }
-});
-
-
-
+//share on sosical media
 function sharePageFB() {
-  // Get the current page URL
   var currentUrl = window.location.href;
-  // Share the URL on social media
   window.open("https://www.facebook.com/sharer/sharer.php?u=" + currentUrl);
 }
 
 function sharePageTwitter() {
-  // Get the current page URL
   var currentUrl = window.location.href;
-  // Share the URL on social media
   window.open("https://twitter.com/share?url=" + currentUrl);
 }
+
+// $('.xxxxxxxxxx').click(function() {
+//   $(this).toggleClass('active');
+// });
+
+
+// var zoom = 1;
+//   function zoomIn() {
+//     zoom += 1;
+//     document.getElementById("myImg").style.transform = "scale(" + zoom + ")";
+//   }
+//   function zoomOut() {
+//     if (zoom > 1) {
+//       zoom -= 1;
+//       document.getElementById("myImg").style.transform = "scale(" + zoom + ")";
+//       return false;
+//     } 
+//   }
+
+// $(document).ready(function(){
+//   $('img').on('click', function(){
+//     var img = $(this).data('img');
+//     $('#modal-img').attr('src', img);
+//   });
+// });
